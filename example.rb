b8bc0105b5c7ef82a0a9a60f2736c53e8e62299a
Jagger Kyne <jagger.kyne@gmail.com>
2015-08-24 16:48:26 +0800

    fixed page reload issue and refactored replacing_words! method

--- example.rb
+++ example.rb
@@ -1,4 +1,3 @@
-# begin # when I put begin here, the code will reload
 def say(msg)
   puts "=> #{msg}"
 end
@@ -18,38 +17,38 @@ def read_content(file)
   end
 end
 
+def replacing_words!(sentence, word, dictionary)
+  sentence.gsub!(word).each do
+    dictionary.sample
+  end
+end
+
 exit_with("No input file") if ARGV.empty?
 exit_with("File does not exist!") if !File.exists?(ARGV[0])
-
 
 nouns_list = read_file("nouns.txt")
 verbs_list = read_file("verbs.txt")
 adjectives_list = read_file("adjectives.txt")
 adverbs_list = read_file("adverbs.txt")
-# begin # when I put begin here, the code will reload
+
 original = read_content("sample.txt")
-# begin # when I put begin here, the code will not reload
+
 dictionary = {noun: nouns_list, verbs: verbs_list, 
                       adjectives: adjectives_list, adverbs: adverbs_list}
 
-# Problem: no new joke will be generated when user typed y.
-# Problem: use regex to match the exact words we target. ADVERBS AND VERBS clash to one another.
-begin # when I put begin here, no joke will not reload
+
+begin
   system "clear"
   puts "Welcome to joke maker!"
-  original.gsub!('NOUN').each do 
-    dictionary[:noun].sample
-  end
-  original.gsub!('VERB').each do
-    dictionary[:verbs].sample
-  end
-  original.gsub!('ADJECTIVE').each do
-    dictionary[:adjectives].sample
-  end
-  original.gsub!('XXX').each do
-    dictionary[:adverbs].sample
-  end
-  puts original
+
+  string_to_modify = original.dup  
+  
+  replacing_words!(string_to_modify, 'NOUN', dictionary[:noun])
+  replacing_words!(string_to_modify, /\bVERB/, dictionary[:verbs])
+  replacing_words!(string_to_modify, 'ADJECTIVE', dictionary[:adjectives])
+  replacing_words!(string_to_modify, /\bADVERB\b/, dictionary[:adverbs])
+
+  puts string_to_modify
   puts "More joke? y/n"
   decision = STDIN.gets.chomp
 end until decision != 'y'
